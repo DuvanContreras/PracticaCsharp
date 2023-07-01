7578b064c6181fc96d5371f233f98064b887bae0
Duvan Contreras <duvanco32@gmail.com>
2023-06-30 19:40:48 -0500

    Pruebas

--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@ namespace PrimeraAplicacion
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Pruebacongit");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Introduzca el primer número");
+            int numero1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo número");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Estos Dos números se sumarán jejeje");
+            Console.WriteLine(" ");
+            int numero3 = numero1 + numero2;
+            Console.WriteLine(numero3);
+
+
+
+
         }
     }
 }       
